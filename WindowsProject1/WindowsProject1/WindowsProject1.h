add82fbc6c414fa4701a3b67549b82408447e5e9
1SYF666 <2832195814@QQ.COM>
2023-10-11 22:06:18 +0800

    first

---
+++ WindowsProject1/WindowsProject1/WindowsProject1.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "resource.h"
+
+
+
+
+
+
+
+
+//*************************************************************************
+
+//#include <afxwin.h>   //mfc头文件、
+
+//class MyApp :public CWinApp       //CWinApp 应用程序类
+//{
+//public:
+//	//程序入口
+//	virtual BOOL InitInstance();
+//
+//};
+//
+//class MyFrame :public CFrameWnd  //窗口框架类
+//{
+//public:
+//	MyFrame();
+//
+//	//声明宏 提供消息的机制
+//	DECLARE_MESSAGE_MAP()
+//
+//	afx_msg void OnLButtonDown(UINT, CPoint);
+//
+//	afx_msg void OnChar(UINT, UINT, UINT);
+//
+//	afx_msg void OnPaint()
+//
+//};
