69280b6db2cefc32b89df8e698b2011aef8dd3b1
1SYF666 <2832195814@QQ.COM>
2023-10-22 23:35:52 +0800

    ten

--- Exercises_C/Exercises_C/exercise.cpp
+++ Exercises_C/Exercises_C/exercise.cpp
@@ -6109,60 +6109,60 @@
 // 与pel2-2a.c一起编译
 // 
 
-void set_mode(int* mode, int* n)
-{
-	if (*mode > 1)
-	{
-		printf("Invalid mode specified. Mode %s used.\n", (0 == *n) ? "0(metric)" : "1(US)");
-	}
-	else
-	{
-		*n = *mode;
-	}
-
-	return;
-
-}
-
-void get_info(int mode, double* range, double* fuel)
-{
-	if (0 == mode)
-	{
-		printf("Enter distance traveled in kilometers: ");
-	}
-	else
-	{
-		printf("Enter distance traveled in miles: ");
-	}
-	scanf("%lf", range);
-
-	if (0 == mode)
-	{
-		printf("Enter fuel consumed in liters: ");
-	}
-	else
-	{
-		printf("Enter fuel consumed in gallons: ");
-	}
-	scanf("%lf", fuel);
-
-	return;
-}
-
-void show_info(int mode, double range, double fuel)
-{
-	if (0 == mode)
-	{
-		printf("Fuel consumption is %.2lf liters per 100 km.\n",
-			(fuel / range) * 100);
-	}
-	else
-	{
-		printf("Fuel consumption is %.1lf miles per gallon.\n",
-			range / fuel);
-	}
-	return;
-}
+//void set_mode(int* mode, int* n)
+//{
+//	if (*mode > 1)
+//	{
+//		printf("Invalid mode specified. Mode %s used.\n", (0 == *n) ? "0(metric)" : "1(US)");
+//	}
+//	else
+//	{
+//		*n = *mode;
+//	}
+//
+//	return;
+//
+//}
+//
+//void get_info(int mode, double* range, double* fuel)
+//{
+//	if (0 == mode)
+//	{
+//		printf("Enter distance traveled in kilometers: ");
+//	}
+//	else
+//	{
+//		printf("Enter distance traveled in miles: ");
+//	}
+//	scanf("%lf", range);
+//
+//	if (0 == mode)
+//	{
+//		printf("Enter fuel consumed in liters: ");
+//	}
+//	else
+//	{
+//		printf("Enter fuel consumed in gallons: ");
+//	}
+//	scanf("%lf", fuel);
+//
+//	return;
+//}
+//
+//void show_info(int mode, double range, double fuel)
+//{
+//	if (0 == mode)
+//	{
+//		printf("Fuel consumption is %.2lf liters per 100 km.\n",
+//			(fuel / range) * 100);
+//	}
+//	else
+//	{
+//		printf("Fuel consumption is %.1lf miles per gallon.\n",
+//			range / fuel);
+//	}
+//	return;
+//}
 
 //int main(void)
 //{
@@ -6188,6 +6188,31 @@ void show_info(int mode, double range, double fuel)
 //	return 0;
 //}
 
+#include <stdio.h>
+
+static int count = 0;
+
+int counter(void);
+
+int main(void)
+{
+    int i, j, k;
+    printf("Please enter a integer: ");
+    scanf("%d", &i);
+
+    for (j = 1; j <= i; j++)
+    {
+        printf("count = %d\n", counter());
+    }
+    printf("The function called %d times.\n", count);
+
+    return 0;
+}
+
+int counter(void)
+{
+    return ++count;
+}
 
 
 
