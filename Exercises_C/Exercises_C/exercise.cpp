b210049fef3edd7ba57b777112b2b9df20afc22c
1SYF666 <2832195814@QQ.COM>
2023-10-26 22:43:18 +0800

    ten

--- Exercises_C/Exercises_C/exercise.cpp
+++ Exercises_C/Exercises_C/exercise.cpp
@@ -6975,45 +6975,46 @@
 //
 //	return 0;
 //}
-
-// 编写一个程序，在顺序在屏幕上显示命令行中列出的所有文件
-
-int main(int argc, char* argv[])
-{
-	int i, temp;
-	FILE* source;
-
-	if (argc == 1)
-	{
-		fprintf(stderr, "Usage: %s filename[s]\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-
-	for (i = 1; i < argc; i++)
-	{
-		if ((source = fopen(argv[i], "r")) == NULL)
-		{
-			fprintf(stderr, "Can't open file %s\n", argv[i]);
-			continue;
-		}
-
-		printf("File %s: \n", argv[i]);
-		//显示
-		while ((temp=getc(source))!=EOF)
-		{
-			putchar(temp);
-		}
-		putchar('\n');
-	
-		if (fclose(source) != 0)
-		{
-			fprintf(stderr, "Can't close file %s\n", argv[i]);
-		}
-	}
-
-
-	return 0;
-}
+//
+//// 编写一个程序，在顺序在屏幕上显示命令行中列出的所有文件
+//
+//int main(int argc, char* argv[])
+//{
+//	int i, temp;
+//	
+//	FILE* source;
+//
+//	if (argc == 1)
+//	{
+//		fprintf(stderr, "Usage: %s filename[s]\n", argv[0]);
+//		exit(EXIT_FAILURE);
+//	}
+//
+//	for (i = 1; i < argc; i++)
+//	{
+//		if ((source = fopen(argv[i], "r")) == NULL)
+//		{
+//			fprintf(stderr, "Can't open file %s\n", argv[i]);
+//			continue;
+//		}
+//
+//		printf("File %s: \n", argv[i]);
+//		//显示
+//		while ((temp=getc(source))!=EOF)
+//		{
+//			putchar(temp);
+//		}
+//		putchar('\n');
+//	
+//		if (fclose(source) != 0)
+//		{
+//			fprintf(stderr, "Can't close file %s\n", argv[i]);
+//		}
+//	}
+//
+//
+//	return 0;
+//}
 
 
 
