8daaacbfd172f4eb91d265c32f3dd7d235ad2662
1SYF666 <2832195814@QQ.COM>
2023-10-27 11:22:33 +0800

    twelve

--- Exercises_C/Exercises_C/exercise.cpp
+++ Exercises_C/Exercises_C/exercise.cpp
@@ -7016,5 +7016,175 @@
 //	return 0;
 //}
 
+/* 反序显示一个文件 */
+
+//#define CNTL_Z '\032' /* DOS文本文件中的文件结尾标记 */
+//#define SLEN 50
+//
+//int main1()
+//{
+//	char file[SLEN];
+//	char ch;
+//	FILE* fp;
+//	long count, last;
+//
+//	puts("Enter the name of the file to be processed: ");
+//	gets_s(file);
+//
+//	if ((fp = fopen(file, "rb")) == NULL)
+//	{
+//		/* 只读和二进制模式 */
+//		printf("reverse can't open %s\n", file);
+//		exit(1);
+//	}
+//
+//	fseek(fp, 0L, SEEK_END); /* 定位到文件结尾出 */
+//
+//	last = ftell(fp);
+//
+//	for (count = 1L; count <= last; count++)
+//	{
+//		fseek(fp, -count, SEEK_END);	/* 回 退 */
+//		ch = getc(fp);
+//
+//		/* 针对DOS，在UNIX下也可工作 */
+//		if (ch != CNTL_Z && ch != '\r')
+//		{
+//			putchar(ch);
+//		}
+//		/* 针对Macintosh */
+//		/*if (ch == '\r')
+//			putchar('\n');
+//		else
+//		{
+//			putchar(ch);
+//		}*/
+//
+//		putchar('\n');
+//		fclose(fp);
+//
+//		return 0;
+//	}
+//
+//	return 0;
+//}
+
+#define BUFSIZE 4096
+#define SLEN 81
+
+void append(FILE* source, FILE* dest)
+{
+	rsize_t bytes;
+	static char temp[BUFSIZE];  // 静态无链接变量只分配一次内存
+
+	/* 
+		fread函数把source指向的文件中的BUFSIZE 块char 类型的
+		数据 写入到temp数组中，返回值是成功读取数据块的数量；
+	*/
+	while ((bytes = fread(temp, sizeof(char), BUFSIZE, source)) > 0)
+	{
+		// fwrite函数把temp数组中bytes块char类型的数据写入dest
+		// 指向的文件中，返回值是成功写入数据块的数量；
+		fwrite(temp, sizeof(char), bytes, dest);
+	}
+
+	return;
+}
+
+
+int main(int argc, char* argv[])
+{
+	int i, ch;
+	int files = 0;
+	FILE* fa;
+	FILE* fs;
+
+	if (argc < 3)
+	{
+		printf("Usage: %s appendfile sourcefile.\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((fa = fopen(argv[1], "a+")) == NULL)
+	{
+		fprintf(stderr, "Can't open %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	if (setvbuf(fa, NULL, _IOFBF, BUFSIZE) != 0)
+	{
+		// 设置缓冲区大小为BUFSIZE；
+		// _IOFBF表示完全缓冲
+		// NULL表示为fa指向的文件分配缓冲区；
+		
+		fputs("Can't create output buffer\n", stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	for (i = 2; i < argc; i++)
+	{
+		if (strcmp(argv[i], argv[1]) == 0)
+		{
+			//防止文件自身附加在文件末尾
+			fputs("Can't append file to itself\n", stderr);
+		}
+		else if ((fs = fopen(argv[i], "r")) == NULL)
+		{
+			fprintf(stderr, "Can't open %s\n", argv[i]);
+		}
+		else
+		{
+			if (setvbuf(fs, NULL, _IOFBF, BUFSIZE) != 0)
+			{
+				// 同上一个setvbuf函数所述；
+				// 本次调用是为了fs指向的文件分配缓冲区；
+				fputs("Can't create input buffer\n", stderr);
+				continue;
+			}
+
+			append(fs, fa);
+
+			if (ferror(fs) != 0)
+			{
+				// 读写出现错误 ferror函数返回一个非0值
+				fprintf(stderr, "Error in reading file %s.\n", argv[i]);
+			}
+
+			if (ferror(fa) != 0)
+			{
+				fprintf(stderr, "Error in writing file %s.\n", argv[1]);
+			}
+
+			fclose(fs);
+			files++;
+			printf("File %s appended.\n", argv[i]);
+		}
+
+	}
+
+	printf("Done appending. %d files appended.\n", files);
+
+	rewind(fa);  // 返回文件起始处；
+
+	printf("%s contents:\n", argv[1]);
+
+	while ((ch=getc(fa))!=EOF)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	puts("Done displaying.");
+
+	fclose(fa);
+
+	return 0;
+}
+
+
+
+
+
 
 
