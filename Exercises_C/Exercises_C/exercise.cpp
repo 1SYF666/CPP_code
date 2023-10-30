f9de28c9ba7083e00445db8a576afce85e344831
1SYF666 <2832195814@QQ.COM>
2023-10-30 20:56:03 +0800

    thirteen

--- Exercises_C/Exercises_C/exercise.cpp
+++ Exercises_C/Exercises_C/exercise.cpp
@@ -7759,29 +7759,164 @@
 //
 
 
+//#define ROWS 20
+//#define COLS 30
+//#define LEVELS 10
+//#define LEN 50
+//
+//const char trans[LEVELS + 1] = " .':~*=&%#";   //存储需要转换的字符;
+//
+//void change(int(*a)[COLS], char(*b)[COLS], int rows)
+//{
+//	int row, col;
+//
+//	for (row = 0; row < ROWS; row++)
+//	{
+//		for (col = 0; col < COLS; col++)
+//		{
+//			// 进行数字对应的字符转换;
+//			b[row][col] = trans[a[row][col]];
+//
+//		}
+//	}
+//
+//	return;
+//}
+//
+//int main(int argc, char* argv[])
+//{
+//	int row, col;
+//	FILE* infile;
+//	char filename[LEN];
+//	int figure[ROWS][COLS];
+//	char picture[ROWS][COLS];
+//
+//	printf("Please enter a filename: ");
+//	scanf("%49s", filename);
+//
+//	if ((infile = fopen(filename, "r+")) == NULL)
+//	{
+//		fprintf(stderr, "Can't open file %s\n", filename);
+//		exit(EXIT_FAILURE);
+//	}
+//
+//	for (row = 0; row < ROWS; row++)
+//	{
+//		for (col = 0; col < COLS; col++)
+//		{
+//			//从 infile 读入到 &figure[row][col]
+//			fscanf(infile, "%d", &figure[row][col]);
+//		}
+//	}
+//
+//	if (ferror(infile))
+//	{
+//		fprintf(stderr, "Error getting data from file %s\n", filename);
+//		exit(EXIT_FAILURE);
+//	}
+//
+//	change(figure, picture, ROWS);
+//
+//	printf("File %s have been transformed pictures:\n", filename);
+//	for (row = 0; row < ROWS; row++)
+//	{
+//		for (col = 0; col < COLS; col++)
+//		{
+//			putchar(picture[row][col]);
+//		}
+//		putchar('\n');
+//	}
+//
+//	fseek(infile, 0L, SEEK_END);  //定位到文件末尾
+//	putc('\n', infile);           //将结果存储到文本文件中
+//
+//	for (row = 0; row < ROWS; row++)
+//	{
+//		for (col = 0; col < COLS; col++)
+//		{
+//			//从 picture 输出到 infile
+//
+//			fprintf(infile, "%c", picture[row][col]);
+//		}
+//		putc('\n', infile);
+//	}
+//
+//	if (fclose(infile) != 0)
+//	{
+//		fprintf(stderr, "Can't close file %s\n", filename);
+//	}
+//
+//	return 0;
+//}
+
+// 消除失真
+// 每个值与它上下左右相邻的值作比较
+// if 该值与其周围相邻值的差都大于1，
+// 则用所有相邻的平均值(四舍五入为整数)代替该值
+// 与边界上相邻的点少于4个，所以做特殊处理
+//
+
+
 #define ROWS 20
 #define COLS 30
 #define LEVELS 10
 #define LEN 50
 
-const char trans[LEVELS + 1] = " .':~*=&%#";   //存储需要转换的字符;
+const char trans[LEVELS + 1] = " .':~*=&%#";  //存储需要转换的字符
 
-void change(int(*a)[COLS], char(*b)[COLS], int rows)
+void change(int rows, int(*a)[COLS], char(*b)[COLS])
 {
 	int row, col;
 
-	for (row = 0; row < ROWS; row++)
+	for (row = 0; row < rows; row++)
 	{
 		for (col = 0; col < COLS; col++)
 		{
-			// 进行数字对应的字符转换;
-			b[row][col] = trans[a[row][col]];
-
+			//*(*(b + row) + col) = trans[*(*(a + row) + col)];
+			*(*(b + row) + col) = *(trans + *(*(a + row) + col));
 		}
+
 	}
 
 	return;
+
 }
+
+int clean_distortion(int(*a)[COLS], int rows, int x, int y)
+{
+	int i = 0;
+	int average = 0;
+	int cnt = 0;
+	int sur = 0;
+
+	int d[4][2] = { {0,1},{1,0},{0,-1},{-1,0} };
+
+	for (i = 0; i < 4; i++)
+	{
+		int dx = x + d[i][0];
+		int dy = y + d[i][1];
+
+		if (dx < 0 || dx >= ROWS || dy < 0 || dy >= COLS)
+		{
+			continue;
+		}
+		++sur;
+
+		if (abs(a[x][y] - a[dx][dy]) > 1)
+		{
+			++cnt;
+			average += a[dx][dy];
+		}
+	}
+
+	if (cnt != sur)
+	{
+		return a[x][y];
+	}
+
+	return 1.0 * average / cnt + 0.5;
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -7804,38 +7939,45 @@ int main(int argc, char* argv[])
 	{
 		for (col = 0; col < COLS; col++)
 		{
-			//从 infile 读入到 &figure[row][col]
 			fscanf(infile, "%d", &figure[row][col]);
 		}
 	}
 
 	if (ferror(infile))
 	{
-		fprintf(stderr, "Error getting data from file %s\n", filename);
+		fprintf(stderr, "Error getting data form file %s\n", filename);
 		exit(EXIT_FAILURE);
 	}
 
-	change(figure, picture, ROWS);
+	for (row = 0; row < ROWS; row++)
+	{
+		for (col = 0; col < COLS; col++)
+		{
+			*(*(figure + row) + col) = clean_distortion(figure, ROWS, row, col);
+		}
+	}
+
+	change(ROWS,figure , picture);
 
 	printf("File %s have been transformed pictures:\n", filename);
+
 	for (row = 0; row < ROWS; row++)
 	{
 		for (col = 0; col < COLS; col++)
 		{
 			putchar(picture[row][col]);
 		}
+
 		putchar('\n');
 	}
 
-	fseek(infile, 0L, SEEK_END);  //定位到文件末尾
-	putc('\n', infile);           //将结果存储到文本文件中
+	fseek(infile, 0L, SEEK_END);   // 定位到文件末尾
+	putc('\n', infile);			   // 将结果存储到文本文件中
 
 	for (row = 0; row < ROWS; row++)
 	{
 		for (col = 0; col < COLS; col++)
 		{
-			//从 picture 输出到 infile
-
 			fprintf(infile, "%c", picture[row][col]);
 		}
 		putc('\n', infile);
@@ -7848,6 +7990,7 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
+
 
 
 
