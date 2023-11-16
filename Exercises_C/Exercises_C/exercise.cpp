23266c2d861b45dff4c2a4fc038128b92f8cea56
1SYF666 <2832195814@QQ.COM>
2023-11-16 14:24:59 +0800

    fifth

--- Exercises_C/Exercises_C/exercise.cpp
+++ Exercises_C/Exercises_C/exercise.cpp
@@ -9634,42 +9634,132 @@
 // 命令行读取两个二进制字符串，对这两个二进制数使用~运算符、&运算符、
 // |运算符和^运算符，并以二进制字符串形式打印结果
 // 
-int bstr_to_dec(const char* str);
+//int bstr_to_dec(const char* str);
+//char* itobs(int n, char* str);
+//int main(int argc, char* argv[])
+//{
+//	char bstr[8 * sizeof(int) + 1];
+//
+//	if (argc != 3)
+//	{
+//		fprintf(stderr, "Usage:%s binarynum1 binarynum2\n", argv[0]);
+//		exit(EXIT_FAILURE);
+//	}
+//
+//	int v1 = bstr_to_dec(argv[1]);
+//	int v2 = bstr_to_dec(argv[2]);
+//
+//	printf("~%s=%s\n", argv[1], itobs(~v1, bstr));
+//	printf("~%s=%s\n", argv[2], itobs(~v2, bstr));
+//	printf("%s & %s=%s\n", argv[1], argv[2], itobs(v1 & v2, bstr));
+//	printf("%s | %s=%s\n", argv[1], argv[2], itobs(v1 | v2, bstr));
+//	printf("%s ^ %s=%s\n", argv[1], argv[2], itobs(v1 ^ v2, bstr));
+//
+//	return 0;
+//
+//}
+//
+//int bstr_to_dec(const char* str)
+//{
+//	int val = 0;
+//
+//	while (*str)
+//	{
+//		val = val * 2 + (*str++) - '0';
+//	}
+//	return val;
+//}
+//
+//char* itobs(int n, char* str)
+//{
+//	int sz = 8 * sizeof(int);
+//
+//	for (int i = sz - 1; i >= 0; --i, n >>= 1)
+//	{
+//		str[i] = (n & 1) + '0';
+//	}
+//	str[sz] = '\0';
+//	return str;
+//}
+
+//*******************2023/11/16 13:56*********************//
+// 接受一个int的参数，并返回该参数打开位的数量
+// 在一个程序中测试该函数
+//
+
+//#define TEST_NUM 13
+//
+//int onbits(int x);
+//char* itobs(int n, char* str);
+//
+//int main(int argc,char* argv[])
+//{
+//	int x = TEST_NUM;
+//
+//	char bstr[8 * sizeof(int) + 1];
+//
+//	printf("%d(%s) has %d bit on.\n", x, itobs(x, bstr), onbits(x));
+//
+//	return 0;
+//}
+//
+//char* itobs(int n, char* str)
+//{
+//	int sz = 8 * sizeof(int);
+//
+//	for (int i = sz - 1; i >= 0; --i, n >>= 1)
+//	{
+//		str[i] = (n & 1) + '0';
+//	}
+//	str[sz] = '\0';
+//
+//	return str;
+//}
+//
+//
+//int onbits(int x)
+//{
+//	int sum = 0;
+//
+//	for (int i = 8 * sizeof(int) - 1; i >= 0; --i)
+//	{
+//		sum += (x >> i) & 1;
+//	}
+//	return sum;
+//}
+
+
+// 编写一个程序，接受两个int 类型的参数：一个是值，一个是位的位置。
+// 如果指定位的位置为1，该函数返回1，否则返回0
+// 在一个程序中测试该函数
+//
+
+#define TEST_NUM 13
+
+int test_bit(int x, int pos);
 char* itobs(int n, char* str);
-int main(int argc, char* argv[])
+
+int main(int argc,char* argv[])
 {
+	int x = TEST_NUM;
+	int sz = 8 * sizeof(int);
 	char bstr[8 * sizeof(int) + 1];
 
-	if (argc != 3)
+	printf("%d(%s):\n", x, itobs(x, bstr));
+
+	for (int i = sz - 1; i >= 0; --i)
 	{
-		fprintf(stderr, "Usage:%s binarynum1 binarynum2\n", argv[0]);
-		exit(EXIT_FAILURE);
+		printf("The position %d is %d\n", i + 1, test_bit(x, i));
 	}
-
-	int v1 = bstr_to_dec(argv[1]);
-	int v2 = bstr_to_dec(argv[2]);
-
-	printf("~%s=%s\n", argv[1], itobs(~v1, bstr));
-	printf("~%s=%s\n", argv[2], itobs(~v2, bstr));
-	printf("%s & %s=%s\n", argv[1], argv[2], itobs(v1 & v2, bstr));
-	printf("%s | %s=%s\n", argv[1], argv[2], itobs(v1 | v2, bstr));
-	printf("%s ^ %s=%s\n", argv[1], argv[2], itobs(v1 ^ v2, bstr));
 
 	return 0;
-
 }
 
-int bstr_to_dec(const char* str)
+int test_bit(int x, int pos)
 {
-	int val = 0;
+	return (x >> pos) & 1;
 
-	while (*str)
-	{
-		val = val * 2 + (*str++) - '0';
-	}
-	return val;
 }
-
 char* itobs(int n, char* str)
 {
 	int sz = 8 * sizeof(int);
@@ -9679,12 +9769,10 @@ char* itobs(int n, char* str)
 		str[i] = (n & 1) + '0';
 	}
 	str[sz] = '\0';
+
 	return str;
+
 }
-
-
-
-
 
 
 
