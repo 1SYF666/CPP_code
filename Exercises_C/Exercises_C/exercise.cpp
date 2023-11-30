48e3e8c39aa34ab0a2cfd62b7e7d3399144945f7
1SYF666 <2832195814@QQ.COM>
2023-11-30 15:43:45 +0800

    fifteen

--- Exercises_C/Exercises_C/exercise.cpp
+++ Exercises_C/Exercises_C/exercise.cpp
@@ -11048,86 +11048,276 @@
 
 /* binbit.c -- 使用位运算显示二进制数 */
 
-char* itobs(int, char*);
+//char* itobs(int, char*);
+//
+//void show_bstr(const char*);
+//
+//int invert_end(int, int);
+//
+//int main()
+//{
+//	char bin_str[8 * sizeof(int) + 1];
+//
+//	int number;
+//
+//	puts("Enter integers and see them in binary.");
+//	puts("Non-numeric input terminates program.");
+//
+//	while (scanf("%d",&number)==1)
+//	{
+//		itobs(number, bin_str);
+//		printf("%d is ", number);
+//		show_bstr(bin_str);
+//		putchar('\n');
+//
+//		number = invert_end(number, 4);
+//
+//		printf("Inverting the last 4 bits gives\n");
+//
+//		show_bstr(itobs(number, bin_str));
+//
+//		putchar('\n');
+//
+//	}
+//
+//	puts("bye!");
+//
+//	return 0;
+//}
+//
+//char* itobs(int n, char* ps)
+//{
+//	int i;
+//
+//	static int size = 8 * sizeof(int);
+//
+//	for (i = size - 1; i >= 0; i--, n >>= 1)
+//	{
+//		ps[i] = (01 & n) + '0';
+//	}
+//
+//	ps[size] = '\0';
+//
+//	return ps;
+//}
+//
+///* 四位一组显示二进制字符串 */
+//void show_bstr(const char* str)
+//{
+//	int i = 0;
+//
+//	while (str[i])
+//	{
+//		putchar(str[i]);
+//		if (++i % 4 == 0 && str[i])
+//		{
+//			putchar(' ');
+//		}
+//	}
+//
+//}
+//
+//int invert_end(int num, int bits)
+//{
+//	int mask = 0;
+//	
+//	int bitval = 1;
+//
+//	while (bits-->0)
+//	{
+//		mask |= bitval;
+//		bitval <<= 1;
+//	}
+//
+//	return num ^ mask;
+//}
 
-void show_bstr(const char*);
 
-int invert_end(int, int);
+//*******************2023/11/30 11:12*********************//
 
-int main()
+/* fields.c -- 定义和使用字段 */
+
+//#define YES 1    // 是否透明，是否可见
+//#define NO 0
+//
+//#define SOLID 0  // 边框线的样式
+//#define DOTTED 1
+//#define DASHED 2
+//
+//#define BLUE 4   // 三原色
+//#define GREEN 2
+//#define RED 1
+//
+//#define BLACK 0
+//#define YELLOW (RED|GREEN)
+//#define MAGENTA (RED|BLUE)
+//#define CYAN (GREEN|BLUE)
+//#define WHITE (RED|GREEN|BLUE)
+//
+//const char* colors[8] = 
+//{ 
+//  "black","red","green","yellow",
+//  "blue","magenta","cyan","white" 
+//};
+//
+//struct box_props
+//{
+//	unsigned int opaque        : 1;
+//	unsigned int fill_color    : 3;
+//	unsigned int               : 4;
+//	unsigned int show_border   : 1;
+//	unsigned int border_color  : 3;
+//	unsigned int border_style  : 3;
+//	unsigned int               : 3;
+//};
+
+/* dualview.c -- 位字段和位运算 */
+#define YES 1    // 是否透明，是否可见
+#define NO 0
+
+#define SOLID 0  // 边框线的样式
+#define DOTTED 1
+#define DASHED 2
+
+#define BLUE 4   // 三原色
+#define GREEN 2
+#define RED 1
+
+#define BLACK 0  // 混合颜色
+#define YELLOW (RED|GREEN)
+#define MAGENTA (RED|BLUE)
+#define CYAN (GREEN|BLUE)
+#define WHITE (RED|GREEN|BLUE)
+
+/* 位运算中使用的常量 */
+#define OPAQUE 0x1
+#define FILL_BLUE 0x8
+#define FILL_GREEN 0x4
+#define FILL_RED 0x2
+#define FILL_MASK 0xE
+
+#define BORDER 0x100
+#define BORDER_BLUE 0x800
+#define BORDER_GREEN 0x400
+#define BORDER_RED 0x200
+#define BORDER_MASK 0xE00
+
+#define B_SOLID 0
+#define B_DOTTED 0x1000
+#define B_DASHED 0x2000
+#define STYLE_MASK 0x3000
+
+const char* colors[8] =
 {
-	char bin_str[8 * sizeof(int) + 1];
+  "black","red","green","yellow",
+  "blue","magenta","cyan","white"
+};
 
-	int number;
+struct box_props
+{
+	unsigned int opaque : 1;
+	unsigned int fill_color : 3;
+	unsigned int : 4;
+	unsigned int show_border : 1;
+	unsigned int border_color : 3;
+	unsigned int border_style : 2;
+	unsigned int : 2;
+};
 
-	puts("Enter integers and see them in binary.");
-	puts("Non-numeric input terminates program.");
+union Views
+{
+	struct box_props st_view;
+	unsigned int ui_view;
+};
 
-	while (scanf("%d",&number)==1)
-	{
-		itobs(number, bin_str);
-		printf("%d is ", number);
-		show_bstr(bin_str);
-		putchar('\n');
 
-		number = invert_end(number, 4);
+void show_settings(const struct box_props* pb);
 
-		printf("Inverting the last 4 bits gives\n");
+void show_settings1(unsigned short);
 
-		show_bstr(itobs(number, bin_str));
+char* itobs(int n, char* ps); /* 把short 值以二进制字符串的形式显示 */
 
-		putchar('\n');
+int main(void)
+{
+	union Views box = { {YES,YELLOW,YES,GREEN,DASHED} };
 
-	}
+	char bin_str[8 * sizeof(unsigned int) + 1];
 
-	puts("bye!");
+	printf("Qriginal  box settings:\n");
+
+	show_settings(&box.st_view);
+
+	printf("\nBox settings using unsigned int view:\n");
+	show_settings1(box.ui_view);
+
+	printf("bits are %s\n", itobs(box.ui_view, bin_str));
+
+	box.ui_view &= ~FILL_MASK;					// 把代表填充色的位开始清零
+	box.ui_view |= (FILL_BLUE | FILL_GREEN);	//重置填冲色
+	box.ui_view ^= OPAQUE;						//转置指示是否透明的位
+	box.ui_view |= BORDER_RED;					//错误的方法
+	box.ui_view &= ~STYLE_MASK;					//清除样式位
+	box.ui_view |= B_DOTTED;					//把样式设置为点
+
+	printf("\nModified box settings:\n");
+	show_settings(&box.st_view);
+	printf("\nBox setting using unsigned int view:\n");
+	show_settings1(box.ui_view);
+	printf("bits are %s\n", itobs(box.ui_view, bin_str));
 
 	return 0;
+}
+
+void show_settings(const struct box_props* pb)
+{
+	printf("Box is %s.\n", pb->opaque == YES ? "opaque" : "transparent");
+	printf("The fill color is %s.\n", colors[pb->fill_color]);
+	printf("Border %s.\n", pb->show_border == YES ? "shown" : "not shown");
+	printf("The border color is %s.\n", colors[pb->border_color]);
+	printf("The border style is ");
+	switch (pb->border_style)
+	{
+	case SOLID: printf("solid.\n"); break;
+	case DOTTED: printf("dotted.\n"); break;
+	case DASHED: printf("dashed.\n"); break;
+	default: printf("unknown type.\n"); break;
+	}
+
+	return;
+}
+
+void show_settings1(unsigned short us)
+{
+	printf("box is %s.\n", us & OPAQUE == OPAQUE ? "opaque" : "transparent");
+	printf("The fill color is %s.\n", colors[(us >> 1) & 7]);
+	printf("Border %s.\n", us & BORDER == BORDER ? "shown" : "not shown");
+
+	printf("The border style is ");
+
+	switch (us & STYLE_MASK)
+	{
+	case B_SOLID: printf("solid.\n"); break;
+	case B_DOTTED: printf("dotted.\n"); break;
+	case B_DASHED: printf("dashed.\n"); break;
+	default: printf("unknown type.\n"); break;
+	}
+
+	printf("The border color is %s.\n", colors[(us >> 9) & 07]);
+
+	return;
 }
 
 char* itobs(int n, char* ps)
 {
 	int i;
-
-	static int size = 8 * sizeof(int);
+	static int size = 8 * sizeof(unsigned int);
 
 	for (i = size - 1; i >= 0; i--, n >>= 1)
 	{
 		ps[i] = (01 & n) + '0';
 	}
-
 	ps[size] = '\0';
 
 	return ps;
 }
 
-/* 四位一组显示二进制字符串 */
-void show_bstr(const char* str)
-{
-	int i = 0;
-
-	while (str[i])
-	{
-		putchar(str[i]);
-		if (++i % 4 == 0 && str[i])
-		{
-			putchar(' ');
-		}
-	}
-
-}
-
-int invert_end(int num, int bits)
-{
-	int mask = 0;
-	
-	int bitval = 1;
-
-	while (bits-->0)
-	{
-		mask |= bitval;
-		bitval <<= 1;
-	}
-
-	return num ^ mask;
-}
