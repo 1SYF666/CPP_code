7c92b3776311ceb1ea612a50360df66afd32b16a
1SYF666 <2832195814@QQ.COM>
2023-12-01 18:02:01 +0800

    sixteen

--- Exercises_C/Exercises_C/exercise.cpp
+++ Exercises_C/Exercises_C/exercise.cpp
@@ -7,6 +7,7 @@
 #include"function.h"
 #include<math.h>
 #include<stdarg.h>
+#include<assert.h>
 
 
 
@@ -11171,153 +11172,240 @@
 //	unsigned int               : 3;
 //};
 
-/* dualview.c -- 位字段和位运算 */
-#define YES 1    // 是否透明，是否可见
-#define NO 0
+///* dualview.c -- 位字段和位运算 */
+//#define YES 1    // 是否透明，是否可见
+//#define NO 0
+//
+//#define SOLID 0  // 边框线的样式
+//#define DOTTED 1
+//#define DASHED 2
+//
+//#define BLUE 4   // 三原色
+//#define GREEN 2
+//#define RED 1
+//
+//#define BLACK 0  // 混合颜色
+//#define YELLOW (RED|GREEN)
+//#define MAGENTA (RED|BLUE)
+//#define CYAN (GREEN|BLUE)
+//#define WHITE (RED|GREEN|BLUE)
+//
+///* 位运算中使用的常量 */
+//#define OPAQUE 0x1
+//#define FILL_BLUE 0x8
+//#define FILL_GREEN 0x4
+//#define FILL_RED 0x2
+//#define FILL_MASK 0xE
+//
+//#define BORDER 0x100
+//#define BORDER_BLUE 0x800
+//#define BORDER_GREEN 0x400
+//#define BORDER_RED 0x200
+//#define BORDER_MASK 0xE00
+//
+//#define B_SOLID 0
+//#define B_DOTTED 0x1000
+//#define B_DASHED 0x2000
+//#define STYLE_MASK 0x3000
+//
+//const char* colors[8] =
+//{
+//  "black","red","green","yellow",
+//  "blue","magenta","cyan","white"
+//};
+//
+//struct box_props
+//{
+//	unsigned int opaque : 1;
+//	unsigned int fill_color : 3;
+//	unsigned int : 4;
+//	unsigned int show_border : 1;
+//	unsigned int border_color : 3;
+//	unsigned int border_style : 2;
+//	unsigned int : 2;
+//};
+//
+//union Views
+//{
+//	struct box_props st_view;
+//	unsigned int ui_view;
+//};
+//
+//
+//void show_settings(const struct box_props* pb);
+//
+//void show_settings1(unsigned short);
+//
+//char* itobs(int n, char* ps); /* 把short 值以二进制字符串的形式显示 */
+//
+//int main(void)
+//{
+//	union Views box = { {YES,YELLOW,YES,GREEN,DASHED} };
+//
+//	char bin_str[8 * sizeof(unsigned int) + 1];
+//
+//	printf("Qriginal  box settings:\n");
+//
+//	show_settings(&box.st_view);
+//
+//	printf("\nBox settings using unsigned int view:\n");
+//	show_settings1(box.ui_view);
+//
+//	printf("bits are %s\n", itobs(box.ui_view, bin_str));
+//
+//	box.ui_view &= ~FILL_MASK;					// 把代表填充色的位开始清零
+//	box.ui_view |= (FILL_BLUE | FILL_GREEN);	//重置填冲色
+//	box.ui_view ^= OPAQUE;						//转置指示是否透明的位
+//	box.ui_view |= BORDER_RED;					//错误的方法
+//	box.ui_view &= ~STYLE_MASK;					//清除样式位
+//	box.ui_view |= B_DOTTED;					//把样式设置为点
+//
+//	printf("\nModified box settings:\n");
+//	show_settings(&box.st_view);
+//	printf("\nBox setting using unsigned int view:\n");
+//	show_settings1(box.ui_view);
+//	printf("bits are %s\n", itobs(box.ui_view, bin_str));
+//
+//	return 0;
+//}
+//
+//void show_settings(const struct box_props* pb)
+//{
+//	printf("Box is %s.\n", pb->opaque == YES ? "opaque" : "transparent");
+//	printf("The fill color is %s.\n", colors[pb->fill_color]);
+//	printf("Border %s.\n", pb->show_border == YES ? "shown" : "not shown");
+//	printf("The border color is %s.\n", colors[pb->border_color]);
+//	printf("The border style is ");
+//	switch (pb->border_style)
+//	{
+//	case SOLID: printf("solid.\n"); break;
+//	case DOTTED: printf("dotted.\n"); break;
+//	case DASHED: printf("dashed.\n"); break;
+//	default: printf("unknown type.\n"); break;
+//	}
+//
+//	return;
+//}
+//
+//void show_settings1(unsigned short us)
+//{
+//	printf("box is %s.\n", us & OPAQUE == OPAQUE ? "opaque" : "transparent");
+//	printf("The fill color is %s.\n", colors[(us >> 1) & 7]);
+//	printf("Border %s.\n", us & BORDER == BORDER ? "shown" : "not shown");
+//
+//	printf("The border style is ");
+//
+//	switch (us & STYLE_MASK)
+//	{
+//	case B_SOLID: printf("solid.\n"); break;
+//	case B_DOTTED: printf("dotted.\n"); break;
+//	case B_DASHED: printf("dashed.\n"); break;
+//	default: printf("unknown type.\n"); break;
+//	}
+//
+//	printf("The border color is %s.\n", colors[(us >> 9) & 07]);
+//
+//	return;
+//}
+//
+//char* itobs(int n, char* ps)
+//{
+//	int i;
+//	static int size = 8 * sizeof(unsigned int);
+//
+//	for (i = size - 1; i >= 0; i--, n >>= 1)
+//	{
+//		ps[i] = (01 & n) + '0';
+//	}
+//	ps[size] = '\0';
+//
+//	return ps;
+//}
 
-#define SOLID 0  // 边框线的样式
-#define DOTTED 1
-#define DASHED 2
+//*******************2023/12/1 12:01*********************//
 
-#define BLUE 4   // 三原色
-#define GREEN 2
-#define RED 1
+////利用宏参数创建字符串：#运算符
+//#define PSQR(x) printf("The square of " #x " is %d.\n",((x)*(x)));
+//
+//int main()
+//{
+//	int y = 5;
+//
+//	PSQR(y);
+//	PSQR(2 + 4);
+//
+//	return 0;
+//}
 
-#define BLACK 0  // 混合颜色
-#define YELLOW (RED|GREEN)
-#define MAGENTA (RED|BLUE)
-#define CYAN (GREEN|BLUE)
-#define WHITE (RED|GREEN|BLUE)
+//// 预处理器的粘合剂：##运算符
+//
+//#define XNAME(n) x##n
+//#define PRINT_XN(n) printf(" x " #n "= %d\n",x##n);
+//
+//int main()
+//{
+//	int XNAME(1) = 14;
+//	int XNAME(2) = 20;
+//	PRINT_XN(1);
+//	PRINT_XN(2);
+//
+//	return 0;
+//}
 
-/* 位运算中使用的常量 */
-#define OPAQUE 0x1
-#define FILL_BLUE 0x8
-#define FILL_GREEN 0x4
-#define FILL_RED 0x2
-#define FILL_MASK 0xE
+//#if SYS==1
+// #include "ibm.h"
+//#elif SYS==2
+// #include "vax.h"
+//#elif SYS==3
+// #include "mac.h"
+//#else
+// #include "general.h"
+//#endif // SYS==1
 
-#define BORDER 0x100
-#define BORDER_BLUE 0x800
-#define BORDER_GREEN 0x400
-#define BORDER_RED 0x200
-#define BORDER_MASK 0xE00
+//int main()
+//{
+//	double x, y, z;
+//	puts("Enter a pair of numbers(0 0 to quit): ");
+//
+//	while (scanf("%lf%lf", &x, &y) == 2&&(x!=0||y!=0))
+//	{
+//		z = x * x - y * y;
+//		assert(z >= 0);
+//		printf("answer is %f\n", sqrt(z));
+//		puts("Next pair of numbers: ");
+//	}
+//
+//	puts("Done");
+//
+//	return 0;
+//}
 
-#define B_SOLID 0
-#define B_DOTTED 0x1000
-#define B_DASHED 0x2000
-#define STYLE_MASK 0x3000
+// mems.c -- 使用memcpy()和memove()函数
+#define SIZE 10
+void show_array(const int ar[], int n);
 
-const char* colors[8] =
+int main()
 {
-  "black","red","green","yellow",
-  "blue","magenta","cyan","white"
-};
+	int values[SIZE] = { 1,2,3,4,5,6,7,8,9,10 };
+	int target[SIZE];
+	double curious[SIZE / 2] = { 1.0,2.0,3.0,4.0,5.0 };
+	puts("memcpy() used: ");
+	puts("value (orignal data): ");
 
-struct box_props
-{
-	unsigned int opaque : 1;
-	unsigned int fill_color : 3;
-	unsigned int : 4;
-	unsigned int show_border : 1;
-	unsigned int border_color : 3;
-	unsigned int border_style : 2;
-	unsigned int : 2;
-};
+	show_array(values, SIZE);
 
-union Views
-{
-	struct box_props st_view;
-	unsigned int ui_view;
-};
-
-
-void show_settings(const struct box_props* pb);
-
-void show_settings1(unsigned short);
-
-char* itobs(int n, char* ps); /* 把short 值以二进制字符串的形式显示 */
-
-int main(void)
-{
-	union Views box = { {YES,YELLOW,YES,GREEN,DASHED} };
-
-	char bin_str[8 * sizeof(unsigned int) + 1];
-
-	printf("Qriginal  box settings:\n");
-
-	show_settings(&box.st_view);
-
-	printf("\nBox settings using unsigned int view:\n");
-	show_settings1(box.ui_view);
-
-	printf("bits are %s\n", itobs(box.ui_view, bin_str));
-
-	box.ui_view &= ~FILL_MASK;					// 把代表填充色的位开始清零
-	box.ui_view |= (FILL_BLUE | FILL_GREEN);	//重置填冲色
-	box.ui_view ^= OPAQUE;						//转置指示是否透明的位
-	box.ui_view |= BORDER_RED;					//错误的方法
-	box.ui_view &= ~STYLE_MASK;					//清除样式位
-	box.ui_view |= B_DOTTED;					//把样式设置为点
-
-	printf("\nModified box settings:\n");
-	show_settings(&box.st_view);
-	printf("\nBox setting using unsigned int view:\n");
-	show_settings1(box.ui_view);
-	printf("bits are %s\n", itobs(box.ui_view, bin_str));
 
 	return 0;
 }
 
-void show_settings(const struct box_props* pb)
-{
-	printf("Box is %s.\n", pb->opaque == YES ? "opaque" : "transparent");
-	printf("The fill color is %s.\n", colors[pb->fill_color]);
-	printf("Border %s.\n", pb->show_border == YES ? "shown" : "not shown");
-	printf("The border color is %s.\n", colors[pb->border_color]);
-	printf("The border style is ");
-	switch (pb->border_style)
-	{
-	case SOLID: printf("solid.\n"); break;
-	case DOTTED: printf("dotted.\n"); break;
-	case DASHED: printf("dashed.\n"); break;
-	default: printf("unknown type.\n"); break;
-	}
 
-	return;
-}
 
-void show_settings1(unsigned short us)
-{
-	printf("box is %s.\n", us & OPAQUE == OPAQUE ? "opaque" : "transparent");
-	printf("The fill color is %s.\n", colors[(us >> 1) & 7]);
-	printf("Border %s.\n", us & BORDER == BORDER ? "shown" : "not shown");
 
-	printf("The border style is ");
 
-	switch (us & STYLE_MASK)
-	{
-	case B_SOLID: printf("solid.\n"); break;
-	case B_DOTTED: printf("dotted.\n"); break;
-	case B_DASHED: printf("dashed.\n"); break;
-	default: printf("unknown type.\n"); break;
-	}
 
-	printf("The border color is %s.\n", colors[(us >> 9) & 07]);
 
-	return;
-}
 
-char* itobs(int n, char* ps)
-{
-	int i;
-	static int size = 8 * sizeof(unsigned int);
 
-	for (i = size - 1; i >= 0; i--, n >>= 1)
-	{
-		ps[i] = (01 & n) + '0';
-	}
-	ps[size] = '\0';
 
-	return ps;
-}
+
 
