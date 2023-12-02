2da72a71b9f63d08b5247216d6a0fa29a6cdfd71
1SYF666 <2832195814@QQ.COM>
2023-12-02 14:57:17 +0800

    sixteen

--- Exercises_C/Exercises_C/exercise.cpp
+++ Exercises_C/Exercises_C/exercise.cpp
@@ -11381,24 +11381,77 @@
 //}
 
 // mems.c -- 使用memcpy()和memove()函数
-#define SIZE 10
-void show_array(const int ar[], int n);
+//#define SIZE 10
+//void show_array(const int ar[], int n)
+//{
+//	int i;
+//	for (i = 0; i < n; i++)
+//	{
+//		printf("%d ", ar[i]);
+//	}
+//	putchar('\n');
+//
+//}
+//
+//int main()
+//{
+//	int values[SIZE] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int target[SIZE];
+//	double curious[SIZE / 2] = { 1.0,2.0,3.0,4.0,5.0 };
+//	puts("memcpy() used: ");
+//	puts("value (orignal data): ");
+//
+//	show_array(values, SIZE);
+//
+//	memcpy(target,values,SIZE*sizeof(int));
+//	puts("target (copy of values): ");
+//	show_array(target, SIZE);
+//
+//	puts("\nUsing memmove() with overlapping ranges: ");
+//	memmove(values + 2, values, 5 * sizeof(int));
+//	puts("value -- elements 0-5 copied to 2-7: ");
+//	show_array(values, SIZE);
+//
+//	puts("\nUsing memmove() to copy double to int: ");
+//	memcpy(target, curious, (SIZE / 2) * sizeof(double));
+//	puts("target -- 5 doubles into 10 int positions: ");
+//	show_array(target, SIZE);
+//
+//	return 0;
+//}
+
+//*******************2023/12/2 14:33*********************//
+double sum(int, ...);
 
 int main()
 {
-	int values[SIZE] = { 1,2,3,4,5,6,7,8,9,10 };
-	int target[SIZE];
-	double curious[SIZE / 2] = { 1.0,2.0,3.0,4.0,5.0 };
-	puts("memcpy() used: ");
-	puts("value (orignal data): ");
+	double s, t;
 
-	show_array(values, SIZE);
+	s = sum(3, 1.1, 2.5, 13.3);
+	t = sum(6, 1.1, 2.1, 13.1, 4.1, 5.1, 6.1);
+	printf("return value for "
+		"sum(3,1.1,2.5,13.3):			%g\n", s);
 
+	printf("return value for "
+		"sum(6, 1.1, 2.1, 13.1, 4.1, 5.1, 6.1): %g\n", t);
 
 	return 0;
 }
 
+double sum(int lim, ...)
+{
+	va_list ap;
+	double tot = 0;
+	int i;
+	va_start(ap, lim);
+	for (i = 0; i < lim; i++)
+	{
+		tot += va_arg(ap, double);  //访问参数列表中的每一个项目
+	}
+	va_end(ap);                     // 清理工作
 
+	return tot;
+}
 
 
 
