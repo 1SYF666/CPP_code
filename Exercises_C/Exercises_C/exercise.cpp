4b9758dc525b745ae4e9ee2dfb9f87111c823515
1SYF666 <2832195814@QQ.COM>
2024-01-05 13:55:17 +0800

    seventh

--- Exercises_C/Exercises_C/exercise.cpp
+++ Exercises_C/Exercises_C/exercise.cpp
@@ -9,6 +9,7 @@
 #include<stdarg.h>
 #include<assert.h>
 #include<iostream>
+#include<vector>
 
 
 
@@ -11807,9 +11808,18 @@ public:
 						 // 所以本函数是必需的
 	
     // cursor 被其类内初始值初始化为0
-	Screen(pos ht, pos wd, char c);
+	Screen(pos ht, pos wd, char c):height(ht),width(wd),contents(ht*wd,c){}
 
+	char get() const                         // 读取光标处的字符
+	{
+		return contents[cursor];             // 隐式内联
+	}
 
+	inline char get(pos ht, pos wd) const;   // 显式内联
+
+	Screen& move(pos r, pos c);
+	Screen& set(char);
+	Screen& set(pos, pos, char);
 
 private:
 	pos cursor = 0;
@@ -11820,8 +11830,44 @@ private:
 };
 
 
+inline                                      // 可以在函数的定义处指定inline
+Screen& Screen::move(pos r, pos c)
+{
+	pos row = r * width;                    // 计算行的位置
+	cursor = row + c;                       // 在行内将光标移动到指定的列
+	return *this;                           // 以左值的形式返回对象
+}
+
+char Screen::get(pos r, pos c) const        // 在类的内部声明成inline
+{
+	pos row = r * width;                    // 计算行的位置
+	return contents[row + c];               // 返回给定列的字符
+}
+
+inline Screen& Screen::set(char c)
+{
+	contents[cursor] = c;
+	
+	return *this;
+}
+
+inline Screen& Screen::set(pos r, pos col, char ch)
+{
+	contents[r * width + col] = ch;
+
+	return *this;
+}
 
 
+
+class Window_mgr
+{
+private:
+	// 这个window_mgr追踪的Screen
+	// 默认情况下，一个window_mgr 包含一个标准尺寸的空白Screen
+	std::vector<Screen>screens{ Screen(24,80,' ') };
+
+};
 
 
 
