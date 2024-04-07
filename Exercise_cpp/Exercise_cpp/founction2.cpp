2b7d5f947a69360e51aeac7f68783345cd70bea8
1SYF666 <99598845+1SYF666@users.noreply.github.com>
2024-04-07 14:58:04 +0800

    seventeenth

--- Exercise_cpp/Exercise_cpp/founction2.cpp
+++ Exercise_cpp/Exercise_cpp/founction2.cpp
@@ -1543,3 +1543,173 @@ void function_14_4(void)
 	cout << "Bye.\n";
 
 }
+
+
+
+abstr_emp::abstr_emp() :fname("no"), lname("one"), job("none")
+{
+
+}
+abstr_emp::abstr_emp(const string& fn, const string& ln, const string& j) : fname(fn), lname(ln), job(j)
+{
+
+}
+
+void abstr_emp::ShowAll()const
+{
+	cout << "First name: " << fname << endl;
+	cout << "last name: " << fname << endl;
+	cout << "Job: " << job << endl;
+}
+
+void abstr_emp::SetAll()
+{
+	cout << "Please enter your first name: ";
+	getline(cin, fname);
+	cout << "Please enter your last name: ";
+	getline(cin, lname);
+	cout << "Please enter your job: ";
+	getline(cin, job);
+}
+
+ostream& operator<<(ostream& os, const abstr_emp& e)
+{
+	os << "First name: " << e.fname << endl;
+	os << "Last name: " << e.lname << endl;
+	os << "Job: " << e.job;
+	return os;
+}
+
+abstr_emp::~abstr_emp()
+{
+
+}
+
+employee::employee() :abstr_emp()
+{
+
+}
+
+employee::employee(const string& fn, const string& ln, const string& j)
+{
+
+}
+
+void employee::ShowAll()const
+{
+	abstr_emp::ShowAll();
+}
+
+void employee::SetAll()
+{
+	abstr_emp::SetAll();
+}
+
+
+manager::manager() :abstr_emp(), inchargeof(0)
+{
+
+}
+manager::manager(const string& fn, const string& ln, const string& j, int ico) : abstr_emp(fn, ln, j), inchargeof(ico)
+{
+
+}
+manager::manager(const abstr_emp& e, int ico) : abstr_emp(e), inchargeof(ico)
+{
+
+}
+manager::manager(const manager& m) : abstr_emp(m), inchargeof(m.inchargeof)
+{
+
+}
+void manager::ShowAll()const
+{
+	abstr_emp::ShowAll();
+	cout << "In charge of: " << inchargeof << endl;
+}
+
+void manager::SetAll()
+{
+	abstr_emp::SetAll();
+	cout << "Please enter a number for inchargerof: ";
+	cin >> inchargeof;
+
+	while (cin.get() != '\n')
+	{
+		continue;
+	}
+
+}
+
+fink::fink() :abstr_emp(), reportsto("none")
+{
+
+}
+fink::fink(const string& fn, const string& ln, const string& j, const string& rpo) : abstr_emp(fn, ln, j), reportsto(rpo)
+{
+
+}
+fink::fink(const abstr_emp& e, const string& rpo) : abstr_emp(e), reportsto(rpo)
+{
+
+}
+fink::fink(const fink& e) : abstr_emp(e), reportsto(e.reportsto)
+{
+
+}
+void fink::ShowAll()const
+{
+	abstr_emp::ShowAll();
+	cout << "Reports to: " << endl;
+}
+void fink::SetAll()
+{
+	abstr_emp::SetAll();
+	cout << "Please enter a string for reportsto: ";
+	getline(cin, reportsto);
+}
+
+highfink::highfink() :abstr_emp(), manager(), fink()
+{
+
+}
+highfink::highfink(const string& fn, const string& ln, const string& j, const string& rpo, int ico) : abstr_emp(fn, ln, j), manager(fn, ln, j, ico), fink(fn, ln, j, rpo)
+{
+
+}
+highfink::highfink(const abstr_emp& e, const string& rpo, int ico) : abstr_emp(e), manager(e, ico), fink(e, rpo)
+{
+
+}
+highfink::highfink(const fink& f, int ico) : abstr_emp(f), manager(f, ico), fink(f)
+{
+
+}
+highfink::highfink(const manager& m, const string& rpo) : abstr_emp(m), manager(m), fink(m, rpo)
+{
+
+}
+highfink::highfink(const highfink& h) : abstr_emp(h), manager(h), fink(h)
+{
+
+}
+void highfink::ShowAll()const
+{
+	abstr_emp::ShowAll();
+	cout << "In charge of: " << manager::InChargeOf() << endl;
+	cout << "Reports to: " << fink::ReportsTo() << endl;
+}
+void highfink::SetAll()
+{
+	abstr_emp::SetAll();
+	cout << "Please enter a number for inchargerof: ";
+	cin >> InChargeOf();
+
+	while (cin.get() != '\n')
+	{
+		continue;
+	}
+
+	cout << "Please enter a string for reportsto: ";
+	getline(cin, ReportsTo());
+}
--- Exercise_cpp/Exercise_cpp/founction2.h
+++ Exercise_cpp/Exercise_cpp/founction2.h
@@ -781,3 +781,105 @@ protected:
 
 
 void function_14_4(void);
+
+
+/*
+	14.5
+	注意，该类层次结构使用了带虚基类的MI，所以要牢记这种情况
+	下用于构造函数初始化列表的特殊规则。还需要注意的是，有些方法被
+	声明为保护的。这可以简化一些highfink方法的代码（例如，如果
+	highfink::ShowAll( )只是调用fink::ShowAll( )和manager::ShwAll( )，则它
+	将调用abstr_emp::ShowAll( )两次）。请提供类方法的实现，并在一个程
+	序中对这些类进行测试。下面是一个小型测试程序
+*/
+
+#ifndef EMP_H_
+#define EMP_H_
+
+class abstr_emp
+{
+public:
+	abstr_emp();
+	abstr_emp(const string& fn, const string& ln, const string& j);
+
+	virtual void ShowAll()const;
+	virtual void SetAll();
+	friend ostream& operator<<(ostream& os, const abstr_emp& e);
+	virtual ~abstr_emp() = 0;
+
+private:
+	string fname;
+	string lname;
+	string job;
+
+};
+
+class employee :public abstr_emp
+{
+public:
+	employee();
+	employee(const string& fn, const string& ln, const string& j);
+	virtual void ShowAll()const;
+	virtual void SetAll();
+
+};
+
+class manager :virtual public abstr_emp
+{
+public:
+	manager();
+	manager(const string& fn, const string& ln, const string& j, int ico = 0);
+	manager(const abstr_emp& e, int ico);
+	manager(const manager& m);
+	virtual void ShowAll()const;
+	virtual void SetAll();
+
+private:
+	int inchargeof;
+
+protected:
+	int InChargeOf()const { return inchargeof; }  // 令派生类能够访问基类成员 
+	int& InChargeOf() { return inchargeof; }		  // 令派生类能够访问基类成员并进行修改 
+
+};
+
+class fink :virtual public abstr_emp
+{
+public:
+	fink();
+	fink(const string& fn, const string& ln, const string& j, const string& rpo);
+	fink(const abstr_emp& e, const string& rpo);
+	fink(const fink& e);
+	virtual void ShowAll()const;
+	virtual void SetAll();
+
+private:
+	string reportsto;
+
+
+protected:
+	const string ReportsTo() const { return reportsto; } // 令派生类能够访问基类成员
+	string& ReportsTo() { return reportsto; }			 // 令派生类能够访问基类成员并进行修改
+
+
+};
+
+class highfink :public manager, public fink
+{
+public:
+	highfink();
+	highfink(const string& fn, const string& ln, const string& j, const string& rpo, int ico);
+	highfink(const abstr_emp& e, const string& rpo, int ico);
+	highfink(const fink& f, int ico);
+	highfink(const manager& m, const string& rpo);
+	highfink(const highfink& h);
+	virtual void ShowAll()const;
+	virtual void SetAll();
+
+private:
+
+};
+#endif // !EMP_H_
+
+void function_14_5(void);
+
